0fe8f2d525f657701501b6c3ad509a7c0bcaec0a
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2022-06-08 10:33:11 -0300

    .

--- src/controllers/joke/delete.joke.controller.js
+++ src/controllers/joke/delete.joke.controller.js
@@ -1,23 +1,41 @@
 
 import prisma from '../../db/prisma'
+import jwt from 'jsonwebtoken'
 
 async function deleteJokeController(request, response){
     
     try {
+
+        const token = await jwt.decode(request.cookies.token, {
+            json: true,
+            complete: true
+        })
+
+        const id = request.params.id
+        const author = token.payload.user
         
        const findJoke = await prisma.joke.findUnique({
            where: {
-               id: request.params.id
+               id
+           },
+           select: {
+               author: {
+                   select: {
+                       username: true
+                   }
+               }
            }
        })
 
        if(!findJoke){
-           return response.send('Piada não encontrada').status(500)
+           return response.send('Piada não encontrada').status(404)
        }
+
+       if(findJoke.author.username !== author) return response.send('Você não pode realizar esta ação').status(400)
        
        const deleteJoke = await prisma.joke.delete({
             where: {
-                id: request.params.id
+                id
             }
         }).finally(async () => {
             await prisma.$disconnect()
