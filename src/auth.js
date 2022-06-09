cde94f9126b98432d5cfe25a8f8dcac7a5d6705d
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2022-06-09 14:55:46 -0300

    .

--- src/auth.js
+++ src/auth.js
@@ -50,7 +50,7 @@ const auth = {
 
         if (username === user.username  &&  password === user.password) {
             let token = auth.getToken(user.username)
-            res.cookie('token', token, { sameSite: 'lax', secure: false})
+            res.cookie('token', token, { sameSite: 'none', secure: true})
             res.status(200).json({ msg: "ok", token })
             return
         } else {
