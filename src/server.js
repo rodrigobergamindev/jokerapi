fc54beecdfed3f538a8f1d36f995e1df2629ca63
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2022-06-09 11:01:09 -0300

    .

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin:'http://localhost:3000',
   credentials: true
-}));
+})); 
 app.use(cookieParser())
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
