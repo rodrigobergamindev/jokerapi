7c4e082ee9aaa0b38984d9345f0820b59b805153
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2022-06-09 11:36:45 -0300

    .

--- src/server.js
+++ src/server.js
@@ -17,6 +17,11 @@ app.use(cors({
   origin:'http://localhost:3000',
   credentials: true
 })); 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cookieParser())
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
