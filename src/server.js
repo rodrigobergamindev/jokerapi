63c13210757f60664425877a8c5cedb6eeaf2794
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2022-06-09 14:25:38 -0300

    .

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin:'https://master--fastidious-kelpie-70580d.netlify.app',
+  origin:'http://localhost:3000',
   credentials: true,
   allowedHeaders: [
     "Access-Control-Allow-Origin", "*",
