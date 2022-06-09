100c309b1dd37fecfdec38e0e4edeb2fd794e6df
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2022-06-09 15:28:58 -0300

    .

--- src/controllers/user/findAllJokes.user.controller.js
+++ src/controllers/user/findAllJokes.user.controller.js
@@ -3,6 +3,7 @@ import prisma from '../../db/prisma'
 async function findAllJokesController(request, response){
     try {
         const username = request.params.username
+        console.log(username)
         const jokes = await prisma.user.findUnique({
             where: {
                 username
