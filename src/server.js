2977d06c012d856faf7a73bd992dd0883c211ef5
rodrigobergamindev <rodrigobergamindev@gmail.com>
2022-06-11 03:48:42 -0300

    .

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin:'https://jokerapi-ustj.netlify.app/',
+  origin:'https://jokerapi-ustj.netlify.app',
   credentials: true,
   allowedHeaders: [
     "Access-Control-Allow-Origin", "*",
